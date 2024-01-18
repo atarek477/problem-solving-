277586805b84d9c381650f57d636c9138df7e652
atarek477 <atarek4777@gmail.com>
2024-01-18 22:07:53 +0200

    happy number

---
+++ src/HappyNumber.java
@@ -0,0 +1,29 @@
+import java.awt.desktop.ScreenSleepEvent;
+
+public class HappyNumber {
+    public static boolean isHappy(int n) {
+        int x;
+        int sum = 0;
+        int count =0;
+    while (count!=100){
+        sum=0;
+        while (n != 0) {
+
+            x = n % 10;
+            //System.out.println(x);
+            n = n / 10;
+            sum = (int) (sum + Math.pow(x, 2));
+
+        }
+        if (sum == 1) {
+            return true;
+        } else {
+            n = sum;
+           // System.out.println(n);
+        }
+count++;
+    }
+        return false;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ import java.util.regex.Pattern;
 public class Main {
 
 
-    public static void main(String[] args) throws FileNotFoundException {
-
+    public static void main(String[] args)  {
+        System.out.println(HappyNumber.isHappy(2));
 
 
     }
