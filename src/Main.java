fe0802f9ef14c1f470de5a3853b02237ca6e25cf
atarek477 <atarek4777@gmail.com>
2024-01-21 20:51:51 +0200

    Merge Sorted Array

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import problems.AddBinary;
+import problems.MergeSortedArray;
 import problems.ReverseBits;
 
 public class Main {
@@ -6,6 +7,10 @@ public class Main {
 
     public static void main(String[] args)  {
       //  AddBinary.addBinary("1","111");
+        int []nums1={};
+        int []nums2={1};
+
+        MergeSortedArray.merge(nums1,0,nums2,1);
 
 
     }
---
+++ src/problems/MergeSortedArray.java
@@ -0,0 +1,55 @@
+package problems;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        if(n==0)
+            return;
+        int i=m;
+        int l=0;
+        int j=n+m;
+        while(i<j){
+            nums1[i]=nums2[l];
+            i++;
+            l++;
+
+        }
+        Arrays.sort(nums1);
+
+
+    }
+
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n==0)
+            return;
+        else if (m==0) {
+            nums1=nums2;
+            return;
+        }
+        int p1  = m-1;
+        int p2  = n-1;
+        int len = nums1.length-1;
+        while(p2!=0||p1!=0){
+           if(nums2[p2]>nums1[p1]){
+               nums1[len]=nums2[p2];
+               p2--;
+               len--;
+           }else {
+               nums1[len]=nums1[p1];
+               p1--;
+               len--;
+
+           }
+        }
+        Arrays.stream(nums1).forEach(System.out::println);
+       // Arrays.sort(nums1);
+
+
+    }
+
+
+
+}
