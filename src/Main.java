22db3f96512b9344e5208eba06e859d098f5f385
atarek477 <atarek4777@gmail.com>
2024-01-22 21:44:32 +0200

    palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 import problems.AddBinary;
 import problems.MergeSortedArray;
+import problems.Palindrome;
 import problems.ReverseBits;
 
 public class Main {
 
 
     public static void main(String[] args)  {
-      //  AddBinary.addBinary("1","111");
-        int []nums1={};
-        int []nums2={1};
-
-        MergeSortedArray.merge(nums1,0,nums2,1);
+        System.out.println(   Palindrome.isPalindrome("Marge, let's \\\"[went].\\\" I await {news} telegram."));
 
 
     }
---
+++ src/problems/Palindrome.java
@@ -0,0 +1,21 @@
+package problems;
+
+import java.util.Arrays;
+
+
+import java.util.stream.Collectors;
+
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        String result= Arrays.stream(s.split("[^a-zA-Z0-9]"))
+                .filter(str->!str.isEmpty() )
+                .collect(Collectors.joining("") )
+                .toLowerCase();
+
+        for (int i = 0; i <result.length() ; i++) {
+            if (result.charAt(i)!=result.charAt(result.length()-i-1))
+                return false;
+        }
+        return true;
+    }
+}
