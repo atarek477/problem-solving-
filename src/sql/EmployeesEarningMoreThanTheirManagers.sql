a59de63048ca47d4c550dda699fd694e9d50871a
atarek477 <atarek4777@gmail.com>
2024-01-23 14:18:13 +0200

    best solution

--- src/sql/EmployeesEarningMoreThanTheirManagers.sql
+++ src/sql/EmployeesEarningMoreThanTheirManagers.sql
@@ -6,3 +6,12 @@ WHERE e1.salary>(SELECT e2.salary
                  WHERE( e2.id=e1.managerId))
 
 
+---best one
+SELECT
+    e1.name AS Employee
+FROM
+    Employee e1
+JOIN
+    Employee e2 ON e1.managerId = e2.id
+WHERE
+    e1.salary > e2.salary;
