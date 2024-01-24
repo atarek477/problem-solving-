de3be08934201fbd6dd012d4ffd493843478dd42
atarek477 <atarek4777@gmail.com>
2024-01-24 17:32:34 +0200

    optimal

--- src/linkedlist/ReverseLinkedList.java
+++ src/linkedlist/ReverseLinkedList.java
@@ -30,6 +30,21 @@ public class ReverseLinkedList {
     }
 
     //optimal
+    public ListNode reverseList(ListNode head) {
+        ListNode curr= head;
+        ListNode prev= null;
+        ListNode next= null;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev ;
+            prev = curr;
+            curr=next;
 
+        }
+
+
+        return prev;
+
+    }
 
 }
