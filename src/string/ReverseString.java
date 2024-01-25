546e9660ca657de2fdb2127e25f399619c127e91
atarek477 <atarek4777@gmail.com>
2024-01-26 00:54:04 +0200

    reverse string in place

--- src/string/ReverseString.java
+++ src/string/ReverseString.java
@@ -1,4 +1,15 @@
 package string;
 
 public class ReverseString {
+
+    public void reverseString(char[] s) {
+        char swap = 0;
+        for (int i = 0; i < (s.length/2); i++) {
+            swap =s[i];
+            s[i]=s[s.length-i-1];
+            s[s.length-i-1]=swap;
+
+
+        }
+    }
 }
