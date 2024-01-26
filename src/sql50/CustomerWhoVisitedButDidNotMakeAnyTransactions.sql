5a3a77dcb5856932f202199d6d60f7e0f34edb30
atarek477 <atarek4777@gmail.com>
2024-01-26 02:12:02 +0200

    customer-who-visited-but-did-not-make-any-transactions

---
+++ src/sql50/CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,6 @@
+SELECT v.customer_id , count(v.customer_id) AS count_no_trans
+FROM visits  AS v
+LEFT JOIN transactions AS t
+ON v.visit_id= t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id
