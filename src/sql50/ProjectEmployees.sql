f08527c27e739de90990b5231b79ea5d123766e8
atarek477 <atarek4777@gmail.com>
2024-01-28 22:02:13 +0200

    project-employees

---
+++ src/sql50/ProjectEmployees.sql
@@ -0,0 +1,6 @@
+SELECT p.project_id ,
+ ROUND(SUM(e.experience_years)/COUNT(e.experience_years),2) AS average_years
+FROM  project   AS p
+JOIN  employee  AS e
+ON p.employee_id =e.employee_id
+GROUP BY p.project_id
