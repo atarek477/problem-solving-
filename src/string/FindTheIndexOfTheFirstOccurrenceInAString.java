b13a96ffea9e46d73c755386f573789424e61260
atarek477 <atarek4777@gmail.com>
2024-01-29 22:15:13 +0200

    find-the-index-of-the-first-occurrence-in-a-string without built in function

--- src/string/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/string/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -6,4 +6,21 @@ public class FindTheIndexOfTheFirstOccurrenceInAString {
         return haystack.indexOf(needle);
 
     }
+
+    public int strStr(String haystack, String needle) {
+        int j=-1;
+        for (int i = 0; i <haystack.length()+1 ; i++) {
+            j++;
+            if(j!=(needle.length())&&
+                    i!=(haystack.length())&&haystack.charAt(i)==needle.charAt(j)){
+                continue;}
+            if(j==needle.length()){
+                return i-j;
+            }
+            i=i-j;
+            j=-1;
+        }
+        return -1;
+    }
+
 }
