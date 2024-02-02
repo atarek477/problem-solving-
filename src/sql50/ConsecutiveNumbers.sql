1f65a711dc5af3a7faa0ad2223a6361555fae573
atarek477 <atarek4777@gmail.com>
2024-02-02 16:11:35 +0200

    Consecutive Numbers optimal solution

--- src/sql50/ConsecutiveNumbers.sql
+++ src/sql50/ConsecutiveNumbers.sql
@@ -14,3 +14,11 @@ ON l2.id2= l1.id+1 AND l2.n = l1.num
 )AS l3
 ON l3.id= l4.id+1 AND l3.num = l4.num
 GROUP BY l4.num ;
+
+
+----> optimal solution real save my time
+SELECT DISTINCT L1.num AS ConsecutiveNums
+FROM logs AS L1, logs AS L2, logs AS L3
+WHERE L1.id = L2.id +1
+AND L1.id = L3.id +2
+AND L1.num = L2.num AND L1.num = L3.num;
