791cb16888ad5f34ede9ec89db2d7b4793d92ad8
atarek477 <atarek4777@gmail.com>
2024-02-11 19:51:28 +0200

    reverse string ii

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import arrays.ContainsDuplicateII;
 import hashtable.FirstUniqueCharacterString;
 import number.PowerofTwo;
 import number.ReverseInt;
+import string.ReverseStringII;
 
 public class Main {
 
 
     public static void main(String[] args)  {
-        System.out.println(FirstUniqueCharacterString.firstUniqChar("leetcode"));
+        System.out.println(ReverseStringII.reverseStr("abcdef",2));
 
 
 
