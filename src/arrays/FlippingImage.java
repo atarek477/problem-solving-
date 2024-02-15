fa42f30fce0960c2e8f160d3971e9187f089c3ca
atarek477 <atarek4777@gmail.com>
2024-02-15 02:09:17 +0200

    Flipping an Image

---
+++ src/arrays/FlippingImage.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class FlippingImage {
+
+    public int[][] flipAndInvertImage(int[][] image) {
+        int row = image.length;
+        int column =image[0].length;
+       int [][]swap = new int[row][column];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <column ; j++) {
+                swap[i][j]=image[i][column-j-1]^1;
+
+            }
+        }
+     return swap;
+    }
+}
---
+++ src/string/AddStrings.java
@@ -0,0 +1,16 @@
+package string;
+
+public class AddStrings {
+
+//    public String addStrings(String num1, String num2) {
+//        for (int i = 0; i < num1.length(); i++) {
+//
+//        }
+//
+//
+//
+//
+//
+//    }
+
+}
