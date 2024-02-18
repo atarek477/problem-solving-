29f6339ca7d9cf9b2717e2ead082910584344eb9
atarek477 <atarek4777@gmail.com>
2024-02-18 18:58:20 +0200

    convert temp

---
+++ src/arrays/ConvertTemperature.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class ConvertTemperature {
+
+    public double[] convertTemperature(double celsius) {
+
+        double[] result= new double[2];
+        result[0] = celsius + 273.15;
+        result[1] = (celsius * 1.80) + 32.00;
+
+        return result;
+
+
+
+
+
+    }
+}
