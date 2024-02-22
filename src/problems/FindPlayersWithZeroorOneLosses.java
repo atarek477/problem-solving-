3d8a8140798b611e7dcd0ea6679fcd75ab4a5b68
atarek477 <atarek4777@gmail.com>
2024-02-22 22:52:21 +0200

    Find Players With Zero or One Losses

--- src/problems/FindPlayersWithZeroorOneLosses.java
+++ src/problems/FindPlayersWithZeroorOneLosses.java
@@ -1,8 +1,36 @@
 package problems;
 
+import java.util.*;
+
 public class FindPlayersWithZeroorOneLosses {
 
+    public List<List<Integer>> findWinners(int[][] matches) {
+        List<List<Integer>> listList = new ArrayList<>();
+        Set<Integer> list1=  new TreeSet<>();
+        Set<Integer> list2 = new TreeSet<>();
+        int row = matches.length;
+        HashMap<Integer,Integer>hashMap= new HashMap<>();
 
+        for (int i = 0; i < row; i++) {
+            if(!hashMap.containsKey(matches[i][1])){
+                hashMap.put(matches[i][1],1);
+                continue;
+            }
+            hashMap.put(matches[i][1],hashMap.get(matches[i][1])+1);
+        }
+
+        for (int i = 0; i < row; i++) {
+            if (!hashMap.containsKey(matches[i][0])) {
+                list1.add(matches[i][0]);
+            }
+            if (hashMap.get(matches[i][1]) == 1) {
+                list2.add(matches[i][1]);
+            }
+        }
+        listList.add(new ArrayList<>(list1));
+        listList.add(new ArrayList<>(list2));
+        return listList;
+    }
 
 
 }
