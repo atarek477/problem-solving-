366bd59b5e7c17c2db276bef238a8a069e081ca6
atarek477 <atarek4777@gmail.com>
2024-03-16 17:08:45 +0200

    Merge Strings Alternately

---
+++ src/string/MergeStringsAlternately.java
@@ -0,0 +1,28 @@
+package string;
+
+public class MergeStringsAlternately {
+
+    public String mergeAlternately(String word1, String word2) {
+
+
+        StringBuilder result= new StringBuilder();
+
+        for (int i = 0; i <Math.max(word1.length(),word2.length()) ; i++) {
+
+            if(i<word1.length())
+                result.append( word1.charAt(i));
+            if(i<word2.length())
+                result.append( word2.charAt(i));
+
+
+
+
+
+        }
+
+       return result.toString();
+    }
+
+
+
+}
