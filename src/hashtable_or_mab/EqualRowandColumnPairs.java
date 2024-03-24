5bc7ee13d10d67b5f980dc6506768d0dc133e8f2
atarek477 <atarek4777@gmail.com>
2024-03-24 21:16:49 +0200

    Equal Row and Column Pairs edit

--- src/hashtable_or_mab/EqualRowandColumnPairs.java
+++ src/hashtable_or_mab/EqualRowandColumnPairs.java
@@ -7,54 +7,29 @@ public class EqualRowandColumnPairs {
 
     public static int equalPairs(int[][] grid) {
         int n = grid.length;
-        HashMap<String,Integer>row= new HashMap<>();
-        HashMap<String,Integer>col= new HashMap<>();
-
-
+        HashMap<String, Integer> row = new HashMap<>();
         String eachCol = "";
         String eachRow = "";
-
-        int count=0;
-        int count1=0;
+        int count = 0;
 
 
-        for (int i = 0; i <n ; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                eachCol= eachCol+grid[j][i]+",";
-                eachRow= eachRow+grid[i][j]+",";
+                eachRow = eachRow + grid[i][j] + ",";
 
             }
-            row.put(eachRow,row.getOrDefault(eachRow,0)+1);
-            col.put(eachCol,col.getOrDefault(eachCol,0)+1);
-
-            eachCol="";
-            eachRow="";
+            row.put(eachRow, row.getOrDefault(eachRow, 0) + 1);
+            eachRow = "";
         }
-        for (int i = 0; i <n ; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                eachCol= eachCol+grid[j][i]+",";
-                eachRow= eachRow+grid[i][j]+",";
+                eachCol = eachCol + grid[j][i] + ",";
 
             }
+            count = count + row.getOrDefault(eachCol, 0);
 
-            if(row.containsKey(eachCol)){
-                count= count+ row.get(eachCol);
-                System.out.println(count1);
-
-
-            }
-            if(col.containsKey(eachRow)){
-                count1= count1+ col.get(eachRow);
-                System.out.println(count1);
-
-
-            }
-            eachCol="";
-            eachRow="";
+            eachCol = "";
         }
-
-
-
-        return Math.max(count1, count);
+        return count;
     }
 }
