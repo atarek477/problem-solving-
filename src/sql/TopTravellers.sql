91cab464a02af19e6736ac60e723147efda7d78d
atarek477 <atarek4777@gmail.com>
2024-03-29 03:10:19 +0200

    Revert "SELECT u.name , (CASE WHEN SUM(r1.distance) IS NULL THEN 0 ELSE SUM(r1.distance) END) AS travelled_distance"
    This reverts commit d40e8732ef0d9133b0a0a2765f428b591ef8e5a7.

--- src/sql/TopTravellers.sql
+++
@@ -1,8 +0,0 @@
-SELECT u.name ,
- (CASE WHEN SUM(r1.distance) IS NULL THEN 0 ELSE SUM(r1.distance) END) AS travelled_distance
-FROM users AS u
-LEFT JOIN rides
-AS r1
-ON r1.user_id=u.id
-GROUP BY r1.user_id
-ORDER BY travelled_distance  DESC , u.name ASC
